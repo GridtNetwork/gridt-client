454d86e8b7a8ec09a2e69417f39847e45ddd90b3
Pieter van Vliet <40736162+PJvanVliet@users.noreply.github.com>
2019-09-25 17:01:56 +0200

    Create mock timeline

---
+++ gridt/src/app/timeline.ts
@@ -0,0 +1,11 @@
+export const timeline = [
+    {
+      name: 'Flossing',
+      description: 'We floss daily to keep our gums happy.',
+      period: '1',
+      leader1: {username: 'Jorn', done: true},
+      leader2: {username: 'Pieter', done: true},
+      leader3: {username: 'Marick', done: true},
+      leader4: {username: 'Yori', done: true}
+    }
+  ];
