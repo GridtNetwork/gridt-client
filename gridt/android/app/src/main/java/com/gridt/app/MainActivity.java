7ddba2c443eb572042058e4e50d3dfdf409036f2
Robin A. Dorstijn <robin@gridt.org>
2020-04-10 11:59:36 +0200

    Update and prepare for android deployment

--- gridt/android/app/src/main/java/com/gridt/app/MainActivity.java
+++ gridt/android/app/src/main/java/com/gridt/app/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.gridt.app;
 
 import android.os.Bundle;
 
@@ -6,6 +6,8 @@ import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
+
+// import com.whitestein.securestorage.SecureStoragePlugin;
 
 public class MainActivity extends BridgeActivity {
   @Override
@@ -16,6 +18,7 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+      // add(SecureStoragePlugin.class);
     }});
   }
 }
