97da6f2ed98a77c771f69997b8b188f4bd63b941
Yori-O92 <45875785+Yori-O92@users.noreply.github.com>
2020-04-18 18:44:31 +0200

    Disable menu when not logged in (#68)
    Fixes #45

--- gridt/src/app/app.component.ts
+++ gridt/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { Router, NavigationStart } from '@angular/router';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { Plugins, AppState, Capacitor } from '@capacitor/core';
 import { AuthService } from './core/auth.service';
+import { Observable } from 'rxjs';
+import { filter, flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit, OnDestroy{
+  public isLoggedIn$: Observable<boolean>;
+
   public appPages = [
     {
       title: 'Home',
@@ -36,6 +40,13 @@ export class AppComponent implements OnInit, OnDestroy{
     private auth: AuthService
   ) {
     this.initializeApp();
+    // this.auth.isLoggedIn$ is a one-shot observable, which means that it is 
+    // not updated when we navigate to another page. Therefore, we look at
+    // navigation events to track the change of this.auth.isLoggedIn$.
+    this.isLoggedIn$ = this.router.events.pipe(
+      filter(event => event instanceof NavigationStart),
+      flatMap(() => this.auth.isLoggedIn$)
+    );
   }
 
   initializeApp() {
