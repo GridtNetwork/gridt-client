60de3141d6dd247691437e9ae3f01c055be0c970
Marick <m.manrho@rug.nl>
2020-09-10 20:50:59 +0200

    Store identity in local storage.

---
+++ gridt/src/app/core/settings.service.spec.ts
@@ -0,0 +1,108 @@
+import { Type } from "@angular/core";
+import { TestBed } from "@angular/core/testing";
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+import { cold } from 'jasmine-marbles';
+import { of, throwError } from "rxjs";
+
+import { AuthService } from './auth.service';
+import { SettingsService } from "./settings.service";
+import { SecureStorageService } from './secure-storage.service';
+
+import { Identity } from "./models/identity.model";
+
+let service: SettingsService;
+let auth: AuthService;
+let secStore: SecureStorageService;
+let authServiceStub: jasmine.SpyObj<AuthService>;
+let secStoreStub: jasmine.SpyObj<SecureStorageService>;
+
+let mock_id: Identity[] = [
+  {
+    id: 1,
+    username: "ALittleOne",
+    bio: "Less then one foot tall.",
+    email: "a_little@one.com",
+    avatar: "arandomstring"
+  }
+]
+const default_headers = {
+  headers: new HttpHeaders({
+    Authorization: "JWT aksdajskd.asdjknaskdn.asdjknakdnasjd"
+  })
+};
+
+class authServiceStub_succes {
+  isLoggedIn$ = of(true);
+  readyAuthentication$ = of(default_headers);
+}
+
+class authServiceStub_failed {
+  isLoggedIn$ = of(false);
+  readyAuthentication$ = throwError("Can't authenticate: no credentials");
+  error_codes = {NOCREDENTIALS: "Can't authenticate: no credentials"};
+}
+
+describe("SettingsService_failed_auth", () => {
+  beforeEach( () => {
+    secStoreStub = jasmine.createSpyObj(
+      'secStore', {'get$': of(mock_id[0]), 'set$': of(true), 'clear$': of(true), 'remove$': of(true)}
+    );
+
+    TestBed.configureTestingModule({
+      providers: [
+        SettingsService,
+        {provide: AuthService, useClass: authServiceStub_failed},
+        {provide: SecureStorageService, useValue: secStoreStub},
+      ]
+    });
+
+    service = TestBed.get(SettingsService as Type<SettingsService>);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+
+  it("should fail to set local identity when not logged in", () => {
+    service.setLocalIdentity(mock_id[0]);
+    expect(secStoreStub.set$).not.toHaveBeenCalled();
+  });
+
+  it("should fail to get local identity when not logged in", () => {
+    expect(service.localIdentity$).toBeObservable(cold('#', null, "Can't authenticate: no credentials"));
+    expect(secStoreStub.get$).not.toHaveBeenCalled();
+  });
+});
+
+describe("SettingsService_succesful_auth", () => {
+  beforeEach( () => {
+    secStoreStub = jasmine.createSpyObj(
+      'secStore', {'get$': of(mock_id[0]), 'set$': of(true), 'clear$': of(true), 'remove$': of(true)}
+    );
+
+    TestBed.configureTestingModule({
+      providers: [
+        SettingsService,
+        {provide: AuthService, useClass: authServiceStub_succes},
+        {provide: SecureStorageService, useValue: secStoreStub},
+      ]
+    });
+
+    service = TestBed.get(SettingsService as Type<SettingsService>);
+  })
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+
+  it("should be able to set the local user Identity", () => {
+    service.setLocalIdentity(mock_id[0]);
+    expect(secStoreStub.set$).toHaveBeenCalledWith("identity", mock_id[0]);
+  })
+
+  it("should be able to get the user's local Identity", () => {
+    expect(service.localIdentity$).toBeObservable(cold('(a|)',{a: mock_id[0]}));
+    expect(secStoreStub.get$).toHaveBeenCalledWith("identity");
+  })
+});
---
+++ gridt/src/app/core/settings.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { Observable, pipe, throwError, of } from "rxjs";
+import { flatMap, tap, map, catchError, pluck } from "rxjs/operators";
+
+import { Identity } from './models/identity.model';
+import { AuthService } from './auth.service';
+import { SecureStorageService } from './secure-storage.service';
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class SettingsService {
+
+  constructor (
+    private auth: AuthService,
+    private secStore: SecureStorageService,
+  ) {}
+
+  /**
+   * Observable to obtain settings from secure storage
+   * If secure storage is empty, this returns an error
+   */
+  public localIdentity$: Observable<Identity> = this.auth.readyAuthentication$.pipe(
+    flatMap(() => this.secStore.get$("identity"))
+  );
+
+  /**
+   * Store settings in the secure storage.
+   * @param identity Settings to be stored.
+   */
+  public setLocalIdentity(identity: Identity): void {
+    console.log(`Storing identity: ${JSON.stringify(identity)} into the local storage.`);
+    let loggedIn: boolean;
+    this.auth.isLoggedIn$.subscribe( (val) => {loggedIn = val;});
+
+    if (loggedIn) {
+      this.secStore.set$("identity", identity).subscribe();
+    }
+  }
+}
