05f51835b2261e6c292a9a3d937336d0e8374901
HendrikPreuter <hendrikpreuter@gmail.com>
2020-09-15 15:06:25 +0100

    Change selector expandable component

--- gridt/src/app/home/expandable/expandable.component.ts
+++ gridt/src/app/home/expandable/expandable.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: "app-expandable",
+  selector: "signal-message",
   templateUrl: "./expandable.component.html",
   styleUrls: ["./expandable.component.scss"],
 })
