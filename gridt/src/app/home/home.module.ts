879e50eaba02b4ff5c67ba95fad011e3e86784dc
HendrikPreuter <hendrikpreuter@gmail.com>
2020-09-23 16:02:00 +0100

    Add style so that both icons stay stationary and renaming of signal-message

--- gridt/src/app/home/home.module.ts
+++ gridt/src/app/home/home.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
-import { SignalMessageComponent } from './expandable/expandable.component'
+import { SignalMessageComponent } from './signal-message/signal-message.component';
 
 @NgModule({
   imports: [
