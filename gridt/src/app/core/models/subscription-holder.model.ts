a78f6ab5843dc9a78a6e49f8727381daecc66b18
HendrikPreuter <hendrikpreuter@gmail.com>
2020-10-01 15:26:08 +0100

    Merge with development

---
+++ gridt/src/app/core/models/subscription-holder.model.ts
@@ -0,0 +1,9 @@
+import { Subscription } from 'rxjs';
+
+export class SubscriptionHolder {
+    protected subscriptions: Subscription[] = [];
+
+    cancelAllSubscriptions () {
+        this.subscriptions.map( subscription => subscription.unsubscribe());
+    }
+}
