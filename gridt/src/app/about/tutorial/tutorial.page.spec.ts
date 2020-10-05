058e6135e518bd5bd52bfe36498187bc98625788
yori <yori@gridt.org>
2020-10-05 12:23:13 +0200

    Tutorial routing test (failing)

--- gridt/src/app/about/tutorial/tutorial.page.spec.ts
+++ gridt/src/app/about/tutorial/tutorial.page.spec.ts
@@ -1,7 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { Location } from "@angular/common"; 
 import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule, ModalController } from '@ionic/angular';
+import { routes } from '../../app-routing.module';
 
 import { TutorialPage } from './tutorial.page';
 
@@ -9,17 +11,27 @@ describe('TutorialPage', () => {
   let component: TutorialPage;
   let fixture: ComponentFixture<TutorialPage>;
   let routerSpy: Router = jasmine.createSpyObj("routerSpy", ['navigate', 'navigateByUrl']);
-  let modalSpy: ModalController = jasmine.createSpyObj("modalSpy", ['create', 'present', 'dismiss']);
+  //let modalSpy: ModalController = jasmine.createSpyObj("modalSpy", ['create', 'present', 'dismiss']);
+  let modalSpy: ModalController = jasmine.createSpyObj("modalSpy", {
+    "create": {
+      "present": function() {
+        return true
+      }},
+    "dismiss": function() {
+      return true
+    }});
+  let router: Router;
+  let location: Location;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TutorialPage ],
       imports: [
         IonicModule.forRoot(),
-        RouterTestingModule
+        RouterTestingModule.withRoutes(routes, {initialNavigation: false})
       ],
       providers: [
-        { provide: Router, useValue: routerSpy },
+        
         { provide: ModalController, useValue: modalSpy }
       ]
     }).compileComponents();
@@ -27,6 +39,9 @@ describe('TutorialPage', () => {
     fixture = TestBed.createComponent(TutorialPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.get(Router);
+    console.log(router);
+    location = TestBed.get(Location);
   }));
 
   it('should create', () => {
@@ -34,8 +49,20 @@ describe('TutorialPage', () => {
   });
 
   //It should navigate to movements if finish is clicked on homepage
-  it('should navigate to movements if finish is called', () => {
-    component.finish();
-    expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('/movements');
+  it('should navigate to movements if finish is called on home page', () => {
+   component.finishTutorial();
+   expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('/movements');
   });
+
+  //It sould dismiss if finish is clicked on the about page
+  it('should dismiss if finish is called on about page', () => {
+    component.finishTutorial();
+    expect(modalSpy.dismiss).toHaveBeenCalled();
+  });
+
+  it('navigate to "" redirects you to /home', fakeAsync(() => {
+    router.navigate(['']);
+    tick();
+    expect(location.path()).toBe('/home');
+  }));
 });
